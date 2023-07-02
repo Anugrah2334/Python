7d256cddb220d605c8257d31e099f4ceadb5ddcc
Anugrah <117362551+Anugrah2334@users.noreply.github.com>
2023-07-02 07:58:08 +0530

    Add files via upload

---
+++ Codes/CAD Lab A,13jan2023.py
@@ -0,0 +1,113 @@
+Python 3.10.9 (tags/v3.10.9:1dd9be6, Dec  6 2022, 20:01:21) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+10+7
+17
+50+40-35
+55
+12*10
+120
+96/12
+8.0
+(-30*4)+500
+380
+78//5
+15
+78%5
+3
+152.78//3.0
+50.0
+152.78%3.0
+2.780000000000001
+5**3
+125
+121**0.5
+11.0
+'HELLO'
+'HELLO'
+"HELLO"
+'HELLO'
+'''HELLO'''
+'HELLO'
+print("The boy replies,\"My name is Aaditya.\"")
+The boy replies,"My name is Aaditya."
+print(R "What\'s ypur name?")
+SyntaxError: invalid syntax
+print(R"What\'s your name?")
+What\'s your name?
+num=7
+amt=123.45
+code='A'
+pi=3.1415926536
+population_of_India=10000000000
+msg="Hi"
+print("NUM="+str(num))
+NUM=7
+print("AMT="+str(amt))
+AMT=123.45
+print("CODE=",+str(code))
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    print("CODE=",+str(code))
+TypeError: bad operand type for unary +: 'str'
+print("MESSAGE="+str(msg))
+MESSAGE=Hi
+print("CODE="+str(code))
+CODE=A
+print("POPULATION OF INDIA="+str(population_of_India))
+POPULATION OF INDIA=10000000000
+Boolean_var=True
+print(Boolean_var)
+True
+20==30
+False
+20!=30
+True
+90<=90
+True
+87<87.0
+False
+"Python"!="Python3.4"
+True
+87==87.0
+True
+>>> 87>=87.0
+True
+>>> name=Anugrah
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    name=Anugrah
+NameError: name 'Anugrah' is not defined
+>>> name="Anugrah"
+>>> age=10
+>>> print(name+"you are"+age+"years old")
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    print(name+"you are"+age+"years old")
+TypeError: can only concatenate str (not "int") to str
+>>> print(name+",you are"+age+"years old")
+Traceback (most recent call last):
+  File "<pyshell#42>", line 1, in <module>
+    print(name+",you are"+age+"years old")
+TypeError: can only concatenate str (not "int") to str
+>>> age="10"
+>>> print(name+"you are"+age+"years old")
+Anugrahyou are10years old
+>>> #This is a comment
+>>> print("Hello") #To display Hello
+Hello
+>>> #Program ends here
+>>> age=21
+>>>    print("You can vote")
+...    
+SyntaxError: unexpected indent
+>>> print("Missile Man of India"+"-Sir APJ Abdul Kalam")
+Missile Man of India-Sir APJ Abdul Kalam
+>>> print("Hello"*5)
+HelloHelloHelloHelloHello
+>>> str='Hello'
+>>> print(str+'4')
+Hello4
+>>> print(str*5)
+HelloHelloHelloHelloHello
+>>> "20"+"30"
+'2030'
