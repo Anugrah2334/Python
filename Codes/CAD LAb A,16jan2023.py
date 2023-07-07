a5d0e7a8f9e05597c6ba812b8696a3ac59e1185f
Anugrah <117362551+Anugrah2334@users.noreply.github.com>
2023-07-07 20:04:20 +0530

    Add files via upload

---
+++ Codes/CAD LAb A,16jan2023.py
@@ -0,0 +1,155 @@
+num1=100
+num2=10
+print("num1+num2=",num1+num2)
+num1+num2= 110
+print("num1-num2=",num1-num2)
+num1-num2= 90
+print("num1*num2=",num1*num2)
+num1*num2= 1000
+print("num1/num2=",num1/num2)
+num1/num2= 10.0
+print("num1%num2=",num1%num2)
+num1%num2= 0
+print(5/2)
+2.5
+print(5//2)
+2
+print(10/2)
+5.0
+print(20//5)
+4
+print(10//2)
+5
+10**2
+100
+pow(10,2)
+100
+10*10
+100
+5**3
+125
+pow(5,3)
+125
+5*5*5
+125
+print(10**2)
+100
+print(5**3)
+125
+a=4
+b=3
+ans=a**b
+print(ans)
+64
+c=5
+d=23
+print(a==b)
+False
+print(c==d)
+False
+print(c!=d)
+True
+print(c<d)
+True
+print(c>d)
+False
+print(c<=d)
+True
+print(c>=d)
+False
+a=10
+b=12
+ans=a|b
+print(ans)
+14
+a=5
+c=9
+ans=a&b
+print(ans)
+4
+a=5
+b=9
+ans=a&b
+print(a&b)
+1
+var=3
+print("value=",~var)
+value= -4
+a=5
+b=9
+ans=a^b
+print(ans)
+12
+#Bitwise operator
+a=10
+b=4
+print("a&b=",a&b)
+a&b= 0
+print("a|b=",a|b)
+a|b= 14
+print("~a=",~a)
+~a= -11
+print("a^b=",a^b)
+a^b= 14
+128>>5
+4
+128>>1
+64
+var=128;
+print("var=",var>>0)
+var= 128
+print("var=",var>>1)
+var= 64
+print("var/2=",var>>1)
+var/2= 64
+print("var/4=",var>>2)
+var/4= 32
+print("var/8=",var>>3)
+var/8= 16
+print("var/16=",var>>4)
+var/16= 8
+print("var/32=",var>>5)
+var/32= 4
+12<<3
+96
+var=2;
+print("var=",var<<0)
+var= 2
+>>> print("var*2=",var<<1)
+var*2= 4
+>>> print("var*4=",var<<2)
+var*4= 8
+>>> print("var*8=",var<<3)
+var*8= 16
+>>> print("var*16=",var<<16)
+var*16= 131072
+>>> print("var*8=",var<<4)
+var*8= 32
+>>> print("var*16=",var<<4)
+...       
+SyntaxError: incomplete input
+>>> print("var*16=",var<<4)
+...       
+var*16= 32
+>>> print("var*32=",var<<5)
+...       
+var*32= 64
+>>> a=5
+...       
+>>> b=10
+...       
+>>> ret=((a<=b) or (a!=b))
+...       
+>>> print("Return value of first expression is",ret)
+...       
+Return value of first expression is True
+>>> ret=((a<b) and (a==b))
+...       
+>>> print("Retuen value of second expression is",ret)
+...       
+Retuen value of second expression is False
+>>> ret=not((a<b) and (a==b))
+...       
+>>> print("Return value of third expression is",ret)
+...       
+Return value of third expression is True
