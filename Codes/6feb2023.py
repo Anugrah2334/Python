42f84850eb8d339add29a0bf3905c10ca492392b
Anugrah <117362551+Anugrah2334@users.noreply.github.com>
2023-07-09 08:53:33 +0530

    Add files via upload

---
+++ Codes/6feb2023.py
@@ -0,0 +1,36 @@
+#PROGRAM1
+def diff(x,y):           #function to subtract two numbers
+    return x-y
+a=20
+b=10
+operation=diff           #function name assign to variable
+print(operation(a,b))    #function called using variable name
+print("\n")
+
+#PROGRAM2
+def my_function():
+    print("Hello from the function")
+my_function()
+print("\n")
+
+#PROGRAM3
+def my_function(fname):
+    print(fname+" Refsnes")
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+print("\n")
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
