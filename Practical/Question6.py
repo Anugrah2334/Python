dbfb76f077f88b096012df307f7909e09f51e479
Anugrah <117362551+Anugrah2334@users.noreply.github.com>
2023-07-30 08:40:09 +0530

    Add files via upload

---
+++ Practical/Question6.py
@@ -0,0 +1,14 @@
+name='NIKUNJ BANSAL'
+sap='500069944'
+dob='13 Oct 1999'
+address='UPES'
+campus='Bidholi Campus'
+pin='248007'
+prog='AI & ML'
+semester='2'
+print('NAME:',name)
+print('\nSAP ID:',sap)
+print('\nDATE OF BIRTH:',dob)
+print('\nADDRESS:',address, '\n\t', campus,'\n\tPincode:',pin)
+print('Programme:',prog)
+print('Semester:',semester)
